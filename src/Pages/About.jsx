8db35ee00f3d7b8856c10557859c4c89974dc8e4
kalin <kalin@KALIN>
2025-05-23 16:58:32 +0800

    que el cv se abra en otra pestaña

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -250,6 +250,8 @@ const AboutPage = () => {
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
               <a
                 href="https://shakugantz-aws-s3.s3.us-east-1.amazonaws.com/CV/CarlosJordan_CV_eng.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-full lg:w-auto"
               >
                 <button
