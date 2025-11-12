8c3dcdbacd04372ce9849720f08d4d2d39693325
Shakugantz <carlosjordanc91@gmail.com>
2025-11-12 10:50:21 +0900

    mejora vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === "serve" ? "/" : "/PortfolioWeb/",
-    plugins: [react()],
-    /*server: {
-    open: true,
-  },*/
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  /*server: {
+  open: true,
+},*/
 });
